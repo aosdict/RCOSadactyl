ede568c666d881f69edbf0eacb31fad10d0403e4
aosdict <aosdict@gmail.com>
2015-03-31 13:19:28 -0400

    Attempt to add event listener to all pages

--- PENTAsdk/lib/main.js
+++ PENTAsdk/lib/main.js
@@ -3,33 +3,48 @@ var data = require("sdk/self").data;
 // file in the "data" directory, and loading the "get-text.js" script
 // into it.
 var text_entry = require("sdk/panel").Panel({
-  contentURL: data.url("text-entry.html"),
-  contentScriptFile: data.url("get-text.js"),
-  height: 18,
-  width: 750,
-  position: {
-    bottom: 5,
-    left: 5
-  }
+    contentURL: data.url("text-entry.html"),
+    contentScriptFile: data.url("get-text.js"),
+    height: 18,
+    width: 750,
+    position: {
+	bottom: 5,
+	left: 5
+    }
 });
 
 // Create a button
 require("sdk/ui/button/action").ActionButton({
-  id: "show-panel",
-  label: "Show Panel",
-  icon: {
-    //"16": "./icon-16.png",
-    "32": "./banana32.png",
-    "64": "./banana.png"
-  },
-  onClick: handleClick
-  // On event: ":"
-
+    id: "show-panel",
+    label: "Show Panel",
+    icon: {
+	//"16": "./icon-16.png",
+	"32": "./banana32.png",
+	"64": "./banana.png"
+    },
+    onClick: handleClick
+    // On event: ":"
 });
+
+// Add an event listener to all opened pages
+require('sdk/page-mod').PageMod({
+    include: ["*", "about:newtab"],
+    contentScriptWhen: "end",
+    contentScript: 'window.alert("AAAAAAAAA");',
+    attachTo: ['top', 'existing']
+});
+
+/*
+require("sdk/tabs").on("ready", function(tab) {
+    tab.attach({
+	contentScript: 'window.alert("Goob gooob");'
+    });
+});
+*/
 
 // Show the panel when the user clicks the button.
 function handleClick(state) {
-  text_entry.show();
+    text_entry.show();
 }
 
 // When the panel is displayed it generated an event called
@@ -37,10 +52,10 @@ function handleClick(state) {
 // send our own "show" event to the panel's script, so the
 // script can prepare the panel for display.
 text_entry.on("show", function() {
-  text_entry.port.emit("show");
+    text_entry.port.emit("show");
 });
 
-text_entry.show();
+//text_entry.show();
 
 // Listen for messages called "text-entered" coming from
 // the content script. The message payload is the text the user
@@ -48,24 +63,24 @@ text_entry.show();
 // In this implementation we'll just log the text to the console.
 
 text_entry.port.on("text-entered", function (text) {
-  // :r  ==  restart
-  if (text === ":r"){
-    // include Components.interface & Components.Classes.  Ci & Cc are just aliases for these.
-    // Ci is used to interact with the window (restart and quit) while Cc is used to call these functions
-    const {Ci, Cc} = require("chrome");
-    var boot = Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup);
-    boot.quit(Ci.nsIAppStartup.eForceQuit|Ci.nsIAppStartup.eRestart);
-  }
-  // :q  == quit
-  else if (text === ":q"){
-    //
-    //
-    const {Ci, Cc} = require("chrome");
-    var boot = Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup);
-    boot.quit(Ci.nsIAppStartup.eForceQuit);
-  }
-  else {
-    console.log(text+" hasn't been implemented yet!");
-  }
+    // :r  ==  restart
+    if (text === ":r"){
+	// include Components.interface & Components.Classes.  Ci & Cc are just aliases for these.
+	// Ci is used to interact with the window (restart and quit) while Cc is used to call these functions
+	const {Ci, Cc} = require("chrome");
+	var boot = Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup);
+	boot.quit(Ci.nsIAppStartup.eForceQuit|Ci.nsIAppStartup.eRestart);
+    }
+    // :q  == quit
+    else if (text === ":q"){
+	//
+	//
+	const {Ci, Cc} = require("chrome");
+	var boot = Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup);
+	boot.quit(Ci.nsIAppStartup.eForceQuit);
+    }
+    else {
+	console.log(text+" hasn't been implemented yet!");
+    }
 
 });
