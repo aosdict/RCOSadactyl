9743f9faa07ca76973b12fa438a34e6dbedf8235
aosdict <aosdict@gmail.com>
2015-04-01 09:55:35 -0400

    Added scrolling

--- PENTAsdk/data/handleKeypress.js
+++ PENTAsdk/data/handleKeypress.js
@@ -2,11 +2,28 @@
 // It will add this universal key handler to the page.
 console.log("Running content script");
 
+const SCROLL_AMT = 10
+
 window.addEventListener("keydown", function(event) {
-    if(event.which == 59) { //semicolon
+    switch(event.which) {
+    case 59: //semicolon
 	self.port.emit("script-message", "Semicolon was pressed");
+	break;
+    case 72: //h = scroll left
+	window.scrollBy(-SCROLL_AMT, 0);
+	break;
+    case 74: //j = scroll down
+	window.scrollBy(0, SCROLL_AMT);
+	break;
+    case 75: //k = scroll up
+	window.scrollBy(0, -SCROLL_AMT);
+	break;
+    case 76: //l = scroll right
+	window.scrollBy(SCROLL_AMT, 0);
+	break;
+    default:
+	console.log("Undefined keydown event " + event.which);
     }
-    console.log("Key down event " + event.which);
 });
 
 window.focus();
