7f6fac74e69355a331363626a70719aa08e6c918
Roastmaster <tripiace@yahoo.com>
2015-04-06 21:46:49 -0400

    Added new features: :nt :nt* :dt

--- PENTAsdk/data/get-text.js
+++ PENTAsdk/data/get-text.js
@@ -2,14 +2,26 @@
 // message to main.js.
 // The message payload is the contents of the edit box.
 var textArea = document.getElementById("edit-box");
+var vimWidnow = document.getElementById("vim-window");
+
 textArea.addEventListener('keyup', function onkeyup(event) {
-  if (event.keyCode == 13) {
+  if (event.keyCode === 13) {
     // Remove the newline.
     text = textArea.value.replace(/(\r\n|\n|\r)/gm,"");
     self.port.emit("text-entered", text);
     textArea.value = '';
   }
 }, false);
+
+// document.addEventListener('1' , function oncolon(event) {
+//  	if (event.keyCode === 49) {
+//  		vimWidnow.show();	
+//  		textArea.show();
+//  		textArea.focus();
+//  		console.log("test4");
+//  	}
+// }, false);
+
 // Listen for the "show" event being sent from the
 // main add-on code. It means that the panel's about
 // to be shown.
@@ -18,4 +30,5 @@ textArea.addEventListener('keyup', function onkeyup(event) {
 // just start typing.
 self.port.on("show", function onShow() {
   textArea.focus();
+
 });
--- PENTAsdk/lib/main.js
+++ PENTAsdk/lib/main.js
@@ -47,7 +47,6 @@ text_entry.show();
 // Listen for messages called "text-entered" coming from
 // the content script. The message payload is the text the user
 // entered.
-// In this implementation we'll just log the text to the console.
 
 text_entry.port.on("text-entered", function (text) {
   // :r  ==  restart
@@ -60,12 +59,92 @@ text_entry.port.on("text-entered", function (text) {
   }
   // :q  == quit
   else if (text === ":q"){
-    //
-    //
     const {Ci, Cc} = require("chrome");
     var boot = Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup);
     boot.quit(Ci.nsIAppStartup.eForceQuit);
   }
+
+  // open a new tab, focus on it
+  else if (text === ":nt" & text.length === 3){
+	const{Ci,Cc} = require("chrome");
+	var mainWindow = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator).getMostRecentWindow("navigator:browser");
+	var gBrowser = mainWindow.gBrowser;
+	gBrowser.selectedTab = gBrowser.addTab("about:blank");
+  }
+
+  // open multiple tabs given a url
+  else if (text.substring(0,3) === ":nt" & text.length > 3){
+  	const{Ci,Cc} = require("chrome");
+  	// parse textArea for the input urls
+  	var urlArray = text.substring(4,text.length).split(" ");
+	var mainWindow = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator).getMostRecentWindow("navigator:browser");
+	var gBrowser = mainWindow.gBrowser;
+
+	// open each new tab iteratively:
+
+	// if the user only inputs a number, open a blank tab that many times
+	// to fixed checks of the first command line argument is a number.
+
+	if (parseInt(urlArray[0]) >= 1 || parseInt(urlArray[0]) <= 10000) {
+		if (urlArray.length === 1) {
+			for (var i = 0 ; i != parseInt(urlArray[0]) ; i++){
+				gBrowser.selectedTab = gBrowser.addTab("about:blank");
+			}
+		}
+		else{
+			for (var i = 0 ; i != parseInt(urlArray[0]) ; i++){
+				gBrowser.selectedTab = gBrowser.addTab(urlArray[1]);
+			}
+		}
+	}
+
+	else {
+		for (var i = 0; i != urlArray.length; i++){
+			console.log(urlArray[i]+'\n');
+			gBrowser.selectedTab = gBrowser.addTab(urlArray[i]);
+		}
+	}
+  }
+
+  // delete tab
+  else if (text === ":dt"){
+  	const {Ci,Cc} = require("chrome");
+  	var mainWindow = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator).getMostRecentWindow("navigator:browser");
+	var gBrowser = mainWindow.gBrowser;
+	gBrowser.removeTab(gBrowser.selectedTab);
+  }
+
+  // delete multiple tabs
+
+  else if (text.substring(0,3) === ":dt"){
+  	const {Ci,Cc} = require("chrome");
+  	var mainWindow = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator).getMostRecentWindow("navigator:browser");
+	var gBrowser = mainWindow.gBrowser;
+
+	if (text.substring(4,6) === "-x"){
+		if (text.length <= 6){
+			gBrowser.removeAllTabsBut(gBrowser.selectedTab);
+		}
+		else {
+			tabsToKeep = text.substring(6, text.length).split(" ");
+		}
+	}
+
+	// check if the user is inputing a url
+  	if (text.indexOf("*") === -1){
+  		
+  		// parse the textArea for the index of tabs to close, starting at 0.
+  		var tabsToClose = text.substring(4, text.length).split(" ");
+  		for (var i = 0 ; i != tabsToClose.length ; i++){
+  			// cast it to an int
+  			targetTab = parseInt(tabsToClose[i]);
+  			// close the corresponding tab
+  			gBrowser.removeTab(gBrowser.mTabContainer.childNodes[targetTab]);
+  		}
+  	}
+  	
+  }
+
   else {
     console.log(text+" hasn't been implemented yet!");
   }
