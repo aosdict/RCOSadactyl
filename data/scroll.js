b9105667d459902767dc8c9095c9b159bf3bbe16
aosdict <aosdict@gmail.com>
2015-05-01 16:49:35 -0400

    Implemented scrolling on pages!

--- data/scroll.js
+++ data/scroll.js
@@ -1,3 +1,39 @@
-self.port.on('scroll-down', function() {
-  //window.scrollBy
+/*self.port.on('scroll-down', function() {
+  window.scrollBy(0,10);
+});
+*/
+
+const SCROLLAMT = 10;
+
+window.addEventListener('keydown', function(event) {
+  if (document.activeElement.tagName != 'INPUT') {
+    switch(event.which) {
+    case 72: // H
+      if (event.shiftKey)
+	window.scrollTo(0, window.pageYOffset);
+      else
+	window.scrollBy(-SCROLLAMT,0);
+      break;
+    case 74: // J
+      if (event.shiftKey)
+	window.scrollTo(window.pageXOffset, document.body.scrollHeight);
+      else
+	window.scrollBy(0, SCROLLAMT);
+      break;
+    case 75: // K
+      if (event.shiftKey)
+	window.scrollTo(window.pageXOffset, 0);
+      else
+	window.scrollBy(0, -SCROLLAMT);
+      break;
+    case 76: // L
+      if (event.shiftKey)
+	window.scrollTo(document.body.scrollWidth, window.pageYOffset);
+      else
+	window.scrollBy(SCROLLAMT, 0);
+      break;
+    default:
+      break;
+    }
+  }
 });
